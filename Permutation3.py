a08e1658bbdd700fba6100f428112eeec52811fb
Bhagyashri Lendave <bhagyshreelendave@gmail.com>
2026-07-21 09:01:33 +0530

    Create Permutation3.py

---
+++ Permutation3.py
@@ -0,0 +1,19 @@
+class Solution:
+    def getPermutation(self, n, k):
+        numbers = []
+        fact = 1
+
+        for i in range(1, n + 1):
+            numbers.append(str(i))
+            fact *= i
+
+        k -= 1
+        result = ""
+
+        for i in range(n, 0, -1):
+            fact //= i
+            index = k // fact
+            result += numbers.pop(index)
+            k %= fact
+
+        return result
